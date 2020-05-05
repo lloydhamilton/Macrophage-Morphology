b848e9c6705dbb7253e0623af5e5433ac4e7a342
lloyd <lloydhamilton1991@gmail.com>
2020-05-05 11:59:52 +0100

    Test macrophage image data

---
+++ ID_Macrophages/testScriptDetecMac.py
@@ -0,0 +1,9 @@
+# from ID_Macrophages import class_detectMacrophages as Dm
+#
+# classInstance = Dm.detectMacrophages('testImage2.png')
+# segmented = classInstance.segmentImage(1)
+import tkinter as tk
+window = tk.Tk()
+window.mainloop()
+
+
