ce0165dd8b74acb32133adde13cab27eec143d79
lloydhamilton1991@gmail.com <Jj!b6jmbser5>
2020-06-03 18:02:24 +0100

    Modified code to define file saving nomenclature and create sub directories

--- ID_Macrophages/class_detectMacrophages.py
+++ ID_Macrophages/class_detectMacrophages.py
@@ -216,19 +216,21 @@ if __name__ == '__main__':
     import pickle
     import os
 
-    classInstance = Dm.detectMacrophages('testImage2.png')
+    file_directory = 'D:\Docs\Python Scripts\Macrophage-Morphology\To Classify\Tile2.png'
+    file_name = file_directory.split('\\')[len(file_directory.split('\\'))-1]
+    classInstance = Dm.detectMacrophages(file_directory)
     segmented = classInstance.segmentImage(0)
     final_logical = classInstance.manual_classification(segmented)
 
     # Save data variable
     path = os.getcwd()
-    savepath = path + '\\Data1'
-    os.mkdir(savepath)
+    savepath = path + '\\ClassifiedImages\\' + file_name.replace('.png', '')
+    os.makedirs(savepath)
 
-    with open((savepath + '\\TrainingLogical1'), 'wb') as f:
+    with open(savepath + '\\Logical_' + file_name.replace('.png', ''), 'wb') as f:
         pickle.dump(final_logical, f)
 
-    with open(savepath + '\\TrainingData1', 'wb') as f:
+    with open(savepath + '\\Data_' + file_name.replace('.png', ''), 'wb') as f:
         pickle.dump(segmented, f)
 
     # with open('Manual Classification Data', 'rb') as f:
