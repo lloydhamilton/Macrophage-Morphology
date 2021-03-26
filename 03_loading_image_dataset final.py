1f749098eb8060a646f6a7f34a0b66d26e5c3369
Lloyd Hamilton <lloydhamilton1991@gmail.com>
2021-03-26 14:56:38 +0000

    Added test line to test command git interface

--- 03_loading_image_dataset final.py
+++ 03_loading_image_dataset final.py
@@ -18,3 +18,5 @@ x_test /= 255
 # Instead, we want each label to be an array with on element set to 1 and and the rest set to 0.
 y_train = keras.utils.to_categorical(y_train, 10)
 y_test = keras.utils.to_categorical(y_test, 10)
+
+#This is a test message
